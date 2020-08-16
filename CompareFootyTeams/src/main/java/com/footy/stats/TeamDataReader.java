45b536df02496ecc3d4e896c82d29b66dbd62dd5
Sridev Balakrishnan <sridev.balakrishnan@gmail.com>
2020-08-17 01:44:15 +0530

    More updates

--- CompareFootyTeams/src/main/java/com/footy/stats/TeamDataReader.java
+++ CompareFootyTeams/src/main/java/com/footy/stats/TeamDataReader.java
@@ -9,6 +9,7 @@ import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
@@ -19,15 +20,14 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.footy.stats.domain.TeamHistory;
@@ -47,7 +47,7 @@ public class TeamDataReader {
 	static String outputWorksheetName = "";
 	static String teamName = "";
 	static int index;
-	
+
 	static List<TeamHistory> teamHistoryLst =  new ArrayList<>();
 
 	private TeamDataReader() {
@@ -55,14 +55,14 @@ public class TeamDataReader {
 	}
 
 	public static void main(String... args) throws IOException {
-		
+
 		Scanner in = new Scanner(System.in);  
 		System.out.print("Enter football team name: ");    
 		teamName = in.nextLine();
 		System.out.print("Enter number of matches to pull stats for: ");    
 		index = Integer.parseInt(in.nextLine());
 		in.close();
-		
+
 		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yy");
 		int win = 0;
 		int draw = 0;
@@ -72,7 +72,7 @@ public class TeamDataReader {
 		int overTotGoals = 0;
 		int goalsFor = 0;
 		int goalsAgainst = 0;
-		
+
 		webPageSurf();
 		teamHistoryLst.sort(Comparator.comparing(
 				TeamHistory::getDate, (s1, s2) -> {
@@ -83,33 +83,33 @@ public class TeamDataReader {
 					}
 					return 0;
 				}));
-		
+
 		if (index > teamHistoryLst.size())
 			index = teamHistoryLst.size();
-		
+
 		for(int i=0; i<index; i++) {
-			
+
 			form.append(teamHistoryLst.get(i).getMatchResult());
-			
+
 			goalsFor = goalsFor + teamHistoryLst.get(i).getHomeGoals();
 			goalsAgainst =  goalsAgainst + teamHistoryLst.get(i).getAwayGoals();
-			
+
 			if (teamHistoryLst.get(i).getMatchResult().equalsIgnoreCase("W"))
 				win++;
 			else if (teamHistoryLst.get(i).getMatchResult().equalsIgnoreCase("D"))
 				draw++;
 			else if (teamHistoryLst.get(i).getMatchResult().equalsIgnoreCase("L"))
 				lost++;
-			
+
 			if (teamHistoryLst.get(i).isBothSidesScored())
 				bothSidesScored++;
-			
+
 			if (teamHistoryLst.get(i).isMoreTotGoals())
 				overTotGoals++;
-			
+
 			log.info(teamHistoryLst.get(i).toString());
 		}
-		
+
 		log.info("[ Team form ] "+form);
 		log.info("[ Match Result ] "+win+"W "+draw+"D "+lost+"L");
 		log.info("[ Total Matches - Both sides scored ] "+bothSidesScored+ "/"+index);
@@ -180,149 +180,11 @@ public class TeamDataReader {
 					(By.xpath("//a[contains(text(),'Fixtures')]//parent::li"))));
 			driver.findElement(By.xpath("//a[contains(text(),'Fixtures')]//parent::li")).click();
 
-			/*driverWait.until(ExpectedConditions.visibilityOf(driver.findElement
-					(By.xpath("//div[contains(@class,'col12-xs-12 col12-s-12 col12-m-12 col12-lg-12 ws-panel')]"))));*/
-			try {
-				driverWait =  new WebDriverWait(driver, 10);
+			driverWait.until(ExpectedConditions.visibilityOf(driver.findElement
+					(By.xpath("//div[contains(@class,'col12-xs-12 col12-s-12 col12-m-12 col12-lg-12 ws-panel')]"))));
 
-				ExpectedCondition<Boolean> expectation = new ExpectedCondition<Boolean>() {
-					public Boolean apply(WebDriver driverjs) {
-						JavascriptExecutor js = (JavascriptExecutor) driverjs;
-						return js.executeScript(websiteLoadCheckString).equals(websiteLoadCheckBoolean);
-					}
-				};
-				driverWait.until(expectation);
-			}
-			catch (TimeoutException e) {
-				log.error("TimeoutException while reading data from stats table: {}", e);
-			}
-			catch (Exception e) {
-				log.error("Exception while waiting to load webpage: {}", e);
-			}
-			
-			try {
-				/*List<WebElement> statsLst = driver.findElements(By.xpath("//div[contains(@class,"
-						+ "'col12-lg-12 col12-m-12 col12-s-12 col12-xs-12 item divtable-row alt')]"));*/
-				
-				/*for(WebElement e: statsLst) {
-				
-					TeamHistory th = new TeamHistory();
-					String homeGoal = null;
-					String awayGoal = null;
-					
-					try {
-						th.setMatchResult(e.findElement(By.xpath(".//div[contains(@class,"
-								+ "'col12-lg-1 col12-m-1 col12-s-1 col12-xs-1 divtable-data form-fixtures')]")).getText());
-					}
-					catch (NoSuchElementException ex) {
-						log.error("Exception while reading elements: {}", ex);
-						continue;
-					}
-					th.setLeague(e.findElement(By.xpath(".//div[contains(@class,"
-							+ "'col12-lg-1 col12-m-1 col12-s-1 col12-xs-1 tournament divtable-data')]")).getText());			    	
-					List<WebElement> rivals = e.findElements(By.xpath(".//div[contains(@class,"
-							+ "'horizontal-match-display team')]"));
-					for(WebElement r: rivals) {
-						if (!r.getText().equalsIgnoreCase(teamName))
-							th.setOpponent(r.getText());
-					}
-					th.setDate(e.findElement(By.xpath(".//div[contains(@class,"
-							+ "'col12-lg-1 col12-m-1 col12-s-0 col12-xs-0 date fourth-col-date divtable-data')]")).getText());
-				
-					String score = e.findElement(By.xpath(".//div[contains(@class,"
-							+ "'col12-lg-1 col12-m-1 col12-s-0 col12-xs-0 divtable-data result')]")).getText();	
-					score = score.replaceAll("\\*", "").replaceAll("\\s", "");
-					String[] scores = score.split(":");
-					for (int i=0 ; i < scores.length; i++) {
-						if (i==0)
-							homeGoal = scores[i];
-						else if (i==1)
-							awayGoal = scores[i];
-					}
-					if ((th.getMatchResult().equalsIgnoreCase("D")) || 
-							((Integer.parseInt(homeGoal) > Integer.parseInt(awayGoal)) && th.getMatchResult().equalsIgnoreCase("W")) ||
-							((Integer.parseInt(homeGoal) < Integer.parseInt(awayGoal)) && th.getMatchResult().equalsIgnoreCase("L"))) {
-						th.setHomeGoals(Integer.parseInt(homeGoal));
-						th.setAwayGoals(Integer.parseInt(awayGoal));
-					}
-					else if (((Integer.parseInt(homeGoal) > Integer.parseInt(awayGoal)) && th.getMatchResult().equalsIgnoreCase("L"))
-							|| ((Integer.parseInt(homeGoal) < Integer.parseInt(awayGoal)) && th.getMatchResult().equalsIgnoreCase("W"))) {
-						th.setHomeGoals(Integer.parseInt(awayGoal));
-						th.setAwayGoals(Integer.parseInt(homeGoal));
-					}
-				
-					if ((Integer.parseInt(homeGoal) > 0)  && (Integer.parseInt(awayGoal) > 0))
-						th.setBothSidesScored(true);
-				
-					if ((Integer.parseInt(homeGoal) + Integer.parseInt(awayGoal)) > 2.5 )
-						th.setMoreTotGoals(true);
-					
-					teamHistoryLst.add(th);
-				}*/
-				
-				List<WebElement> statsLst = driver.findElements(By.xpath("//div[contains(@class,"
-						+ "'col12-lg-12 col12-m-12 col12-s-12 col12-xs-12 item divtable-row ')]"));
-				
-				for(WebElement e: statsLst) {
 
-					TeamHistory th = new TeamHistory();
-					String homeGoal = null;
-					String awayGoal = null;
-					
-					try {
-						th.setMatchResult(e.findElement(By.xpath(".//div[contains(@class,"
-							+ "'col12-lg-1 col12-m-1 col12-s-1 col12-xs-1 divtable-data form-fixtures')]")).getText());
-					}
-					catch (NoSuchElementException ex) {
-						log.error("Exception while reading elements: {}", ex);
-						continue;
-					}
-					th.setLeague(e.findElement(By.xpath(".//div[contains(@class,"
-							+ "'col12-lg-1 col12-m-1 col12-s-1 col12-xs-1 tournament divtable-data')]")).getText());
-					List<WebElement> rivals = e.findElements(By.xpath(".//div[contains(@class,"
-							+ "'horizontal-match-display team')]"));
-					for(WebElement r: rivals) {
-						if (!r.getText().equalsIgnoreCase(teamName))
-							th.setOpponent(r.getText());
-					}
-					th.setDate(e.findElement(By.xpath(".//div[contains(@class,"
-							+ "'col12-lg-1 col12-m-1 col12-s-0 col12-xs-0 date fourth-col-date divtable-data')]")).getText());
-
-					String score = e.findElement(By.xpath(".//div[contains(@class,"
-							+ "'col12-lg-1 col12-m-1 col12-s-0 col12-xs-0 divtable-data result')]")).getText();
-					score = score.replaceAll("\\*", "").replaceAll("\\s", "");
-					String[] scores = score.split(":");
-					for (int i=0 ; i < scores.length; i++) {
-						if (i==0)
-							homeGoal = scores[i];
-						else if (i==1)
-							awayGoal = scores[i];
-					}
-					if ((th.getMatchResult().equalsIgnoreCase("D")) || 
-							((Integer.parseInt(homeGoal) > Integer.parseInt(awayGoal)) && th.getMatchResult().equalsIgnoreCase("W")) ||
-							((Integer.parseInt(homeGoal) < Integer.parseInt(awayGoal)) && th.getMatchResult().equalsIgnoreCase("L"))) {
-						th.setHomeGoals(Integer.parseInt(homeGoal));
-						th.setAwayGoals(Integer.parseInt(awayGoal));
-					}
-					else if (((Integer.parseInt(homeGoal) > Integer.parseInt(awayGoal)) && th.getMatchResult().equalsIgnoreCase("L"))
-							|| ((Integer.parseInt(homeGoal) < Integer.parseInt(awayGoal)) && th.getMatchResult().equalsIgnoreCase("W"))) {
-						th.setHomeGoals(Integer.parseInt(awayGoal));
-						th.setAwayGoals(Integer.parseInt(homeGoal));
-					}
-					
-					if ((Integer.parseInt(homeGoal) > 0)  && (Integer.parseInt(awayGoal) > 0))
-						th.setBothSidesScored(true);
-
-					if ((Integer.parseInt(homeGoal) + Integer.parseInt(awayGoal)) > 2.5 )
-						th.setMoreTotGoals(true);
-					
-					teamHistoryLst.add(th);
-				}
-
-			}
-			catch (NoSuchElementException e) {
-				log.error("Exception while reading elements: {}", e);
-			}
+			populateData(driver);
 
 		}
 		catch (TimeoutException e) {
@@ -334,8 +196,121 @@ public class TeamDataReader {
 		}
 		finally {
 			log.info("Driver closed");			
-			//driver.close();
+			driver.close();
 		}
+	}
+
+	private static void populateData(WebDriver driver) {
+		try {
+			List<WebElement> statsLst = driver.findElements(By.xpath("//div[contains(@class,"
+					+ "'col12-lg-12 col12-m-12 col12-s-12 col12-xs-12 item divtable-row ')]"));
+
+			// Obtain home team name
+			Select select = new Select(driver.findElement(By.id("teams")));
+			WebElement option = select.getFirstSelectedOption();
+			String homeTeam = option.getText();
+
+			for(WebElement e: statsLst) {
+
+				TeamHistory th = new TeamHistory();
+				int iHomeGoal = 0;
+				int iAwayGoal = 0;
+
+				try {
+					th.setMatchResult(e.findElement(By.xpath(".//div[contains(@class,"
+							+ "'col12-lg-1 col12-m-1 col12-s-1 col12-xs-1 divtable-data form-fixtures')]")).getText());
+				}
+				catch (NoSuchElementException ex) {
+					log.error("Exception while reading elements: {}", ex);
+					continue;
+				}
+				
+				// Populate league in bean
+				th.setLeague(retrieveLeague(e));
+				
+				// Populate opponent name in bean
+				th.setOpponent(retrieveOpponent(e, homeTeam));
+				
+				// Populate match date in bean
+				th.setDate(retrieveMatchDate(e));
+				
+				HashMap<Integer, Integer> goals = retrieveGoals(e, th);
+				iHomeGoal = goals.get(0);
+				iAwayGoal = goals.get(1);
+				th.setHomeGoals(iHomeGoal);
+				th.setAwayGoals(iAwayGoal);
+
+				if ((iHomeGoal > 0)  && (iAwayGoal > 0))
+					th.setBothSidesScored(true);
+
+				if ((iHomeGoal + iAwayGoal) > 2.5 )
+					th.setMoreTotGoals(true);
+
+				teamHistoryLst.add(th);
+			}
+		}
+		catch (NoSuchElementException e) {
+			log.error("Exception while reading elements: {}", e);
+		}
+	}
+
+	private static String retrieveLeague(WebElement e) {		
+
+		return (e.findElement(By.xpath(".//div[contains(@class,"
+				+ "'col12-lg-1 col12-m-1 col12-s-1 col12-xs-1 tournament divtable-data')]")).getText());
+	}
+
+	private static String retrieveOpponent(WebElement e, String homeTeam) {		
+
+		String rival = "";
+		List<WebElement> teams = e.findElements(By.xpath(".//div[contains(@class,"
+				+ "'horizontal-match-display team')]"));
+		for(WebElement r: teams) {
+			if (!org.apache.commons.lang3.StringUtils.containsIgnoreCase(r.getText(), homeTeam)) {
+				rival = r.getText().replaceAll("\\d","");
+			}
+		}
+		return rival;
+	}
+
+	private static String retrieveMatchDate(WebElement e) {		
+
+		return (e.findElement(By.xpath(".//div[contains(@class,"
+				+ "'col12-lg-1 col12-m-1 col12-s-0 col12-xs-0 date fourth-col-date divtable-data')]")).getText());	
+	}
+	
+	private static HashMap<Integer, Integer> retrieveGoals(WebElement e, TeamHistory th) {
+		
+		HashMap<Integer, Integer> goals = new HashMap<>();
+		String homeGoal = null;
+		String awayGoal = null;
+		int iHomeGoal = 0;
+		int iAwayGoal = 0;
+		int iTempGoal = 0;
+		
+		String score = e.findElement(By.xpath(".//div[contains(@class,"
+				+ "'col12-lg-1 col12-m-1 col12-s-0 col12-xs-0 divtable-data result')]")).getText();
+		score = score.replaceAll("\\*", "").replaceAll("\\s", "");
+		String[] scores = score.split(":");
+		for (int i=0 ; i < scores.length; i++) {
+			if (i==0)
+				homeGoal = scores[i];
+			else
+				awayGoal = scores[i];
+		}
+		
+		iHomeGoal = Integer.parseInt(homeGoal);
+		iAwayGoal = Integer.parseInt(awayGoal);
+		
+		if (((iHomeGoal > iAwayGoal) && th.getMatchResult().equalsIgnoreCase("L"))
+				|| ((iHomeGoal < iAwayGoal) && th.getMatchResult().equalsIgnoreCase("W"))) {
+			iTempGoal = iHomeGoal;
+			iHomeGoal = iAwayGoal;
+			iAwayGoal = iTempGoal;
+		}
+		goals.put(0, iHomeGoal);
+		goals.put(1, iAwayGoal);
+		return goals;
 	}
 
 }
